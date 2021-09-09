fa44cb75e606e79f0cd074cda4fa0343fd02cd18
Dmitri Osipov <dmitri.osipov@lexistems.com>
2021-09-09 18:22:13 +0300

    minor corrections

--- layers/decoder_layer.py
+++ layers/decoder_layer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from layers.fnet_layer import FNetEncoderLayer, FNetLayer
+from layers.fnet_layer import FNetEncoderLayer
 from layers.pff import PositionwiseFeedforwardLayer
 
 class DecoderLayer(nn.Module):
